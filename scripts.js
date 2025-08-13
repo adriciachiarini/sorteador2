f2df5e38c4ecb7d401a903cb96eb02f59e331828
adriciachiarini <adricia.dev@gmail.com>
2025-08-13 18:17:08 -0300

    Primeiro commit

---
+++ scripts.js
@@ -0,0 +1,8 @@
+function generateNumber() {
+    const min = Math.ceil(document.querySelector('.input-min').value)
+    const max = Math.floor(document.querySelector('.input-max').value)
+
+    const result = Math.floor(Math.random() * (max - min + 1)) + min;
+    document.getElementById('result').innerHTML = 'O número sorteado é: ' + result;
+
+}
